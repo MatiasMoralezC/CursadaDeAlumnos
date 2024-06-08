5a23f607f6100984f1d1b3b8d75c640f3e227550
MatiasMoralezC <matiasmoralezc@gmail.com>
2024-06-08 17:40:51 -0300

    Nuevas tablas

--- pruebatp.sql
+++ pruebatp.sql
@@ -23,7 +23,7 @@ CREATE TABLE correlatividad(
 		id_mat_correlativa INTEGER,
 		PRIMARY KEY (id_materia, id_mat_correlativa),
 		FOREIGN KEY (id_materia) REFERENCES materia (id_materia),
-		FOREIGN KEY (id_mat_correlativa) REFERENCES materia _id_materia)
+		FOREIGN KEY (id_mat_correlativa) REFERENCES materia (id_materia)
 );
 
 CREATE TABLE comision(
@@ -32,4 +32,36 @@ CREATE TABLE comision(
 		cupo INTEGER,
 		PRIMARY KEY (id_materia, id_comision),
 		FOREIGN KEY (id_materia) REFERENCES materia (id_materia)
+);
+
+CREATE TABLE cursada (
+	id_materia INTEGER,
+	id_alumne INTEGER,
+	id_comision INTEGER,
+	f_inscripcion TIMESTAMP,
+	nota INTEGER,
+	estado CHAR(12),
+	PRIMARY KEY (id_materia, id_alumno),
+	FOREIGN KEY (id_materia) REFERENCES materia (id_materia),
+	FOREIGN KEY (id_alumne) REFERENCES alumne (id_alumne)
+);
+
+CREATE TABLE periodo(
+	semestre INTEGER PRIMARY KEY,
+	estado CHAR(12)
+);
+
+CREATE TABLE historia_academica(
+	id_alumne INTEGER,
+	semestre TEXT,
+	id_materia INTEGER,
+	id_comision INTEGER,
+	estado CHAR(15),
+	nota_regular INTEGER,
+	nota_final INTEGER,
+	PRIMARY KEY (id_alumne, semestre, id_materia),
+	FOREIGN KEY (id_alumne) REFERENCES alumne (id_alumne),
+	FOREIGN KEY (semestre) REFERENCES periodo (semestre),
+	FOREIGN KEY (id_materia) REFERENCES materia (id_materia),
+	FOREIGN KEY (id_comision) REFERENCES comision (id_comision)
 );
