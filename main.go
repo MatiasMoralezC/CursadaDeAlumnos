3dec3329cf0b6aead508b713fbc5e05e521e59ed
rmontoro94 <>
2024-06-20 14:45:21 -0300

    se actualizan las funciones agregar y borrar  foreign y primary keys

--- main.go
+++ main.go
@@ -1,363 +1,502 @@
 package main
 
 import (
-	"database/sql"
-	_ "github.com/lib/pq"
-	"log"
-	"fmt"
-	"io/ioutil"
-	"encoding/json"
-	"os"
-	"os/exec"
+"database/sql"
+_ "github.com/lib/pq"
+"log"
+"fmt"
+"io/ioutil"
+"encoding/json"
+"os"
+"os/exec"
 )
 
 type Alumne struct {
-	Id_alumne	int
-	Nombre		string
-	Apellido	string
-	Dni			int
-	Fecha_nacimiento	string
-	Telefono	string
-	Email		string
+Id_alumne int
+Nombre string
+Apellido string
+Dni int
+Fecha_nacimiento string
+Telefono string
+Email string
 }
 
 type Materia struct {
-	Id_materia	int
-	Nombre	string
+Id_materia int
+Nombre string
 }
 
 type Comision struct {
-	Id_materia 	int
-	Id_comision int
-	Cupo 		int
-	}
+Id_materia int
+Id_comision int
+Cupo int
+}
 
 type Correlatividad struct {
-	Id_materia	int
-	Id_mat_correlativa	int
+Id_materia int
+Id_mat_correlativa int
 }
 
 type Cursada struct {
-	Id_materia	int
-	Id_alumne	int
-	Id_comision	int
-	F_inscripcion	string
-	Nota		int
-	Estado		string
+Id_materia int
+Id_alumne int
+Id_comision int
+F_inscripcion string
+Nota int
+Estado string
 }
 
 type Periodo struct {
-	Semestre	string
-	Estado	string
+Semestre string
+Estado string
 }
 
 type Historia_academica struct {
-	Id_alumne	int
-	Semestre	string
-	Id_materia	int
-	Id_comision	int
-	Estado	string
-	Nota_regular	int
-	Nota_final	int
+Id_alumne int
+Semestre string
+Id_materia int
+Id_comision int
+Estado string
+Nota_regular int
+Nota_final int
 }
 
 type Error struct {
-	Id_error	int
-	Operacion	string
-	Semestre	string
-	Id_alumne	int
-	Id_materia	int
-	Id_comision	int
-	F_error		string
-	Motivo		string
+Id_error int
+Operacion string
+Semestre string
+Id_alumne int
+Id_materia int
+Id_comision int
+F_error string
+Motivo string
 }
 
 type Envio_mail struct {
-	Id_email	int
-	F_generacion	string
-	Email_alumne	string
-	Asunto		string
-	Cuerpo		string
-	F_envio		string
-	Estado		string
+Id_email int
+F_generacion string
+Email_alumne string
+Asunto string
+Cuerpo string
+F_envio string
+Estado string
 }
 
 func main() {
-	ejecutarPrograma()
+ejecutarPrograma()
 }
 
 func mostrarOpciones() int {
-	fmt.Printf ("Elige una opcion para continuar:\n")
-	fmt.Printf ("Para crear la DB, escriba el nùmero 1\n")
-	fmt.Printf ("Para crear las tablas de la DB, escriba el nùmero 2\n")
-	fmt.Printf ("Para cargar los datos de los archivos JSON, escriba el nùmero 3\n")
-	fmt.Printf ("Para agregar las primary keys, escriba el nùmero 4\n")
-	fmt.Printf ("Para agregar las foreign keys, escriba el nùmero 5\n")
-	fmt.Printf ("Para realizar la inscripciòn a una materia, escriba el nùmero 6\n")
-	fmt.Printf ("Para salir, escriba el nùmero 7\n")
-	
-	var opcion int
-	fmt.Scanf("%d",&opcion)
-	return opcion
+fmt.Printf ("Elige una opcion para continuar:\n")
+fmt.Printf ("Para crear la DB, escriba el nùmero 1\n")
+fmt.Printf ("Para crear las tablas de la DB, escriba el nùmero 2\n")
+fmt.Printf ("Para cargar los datos de los archivos JSON, escriba el nùmero 3\n")
+fmt.Printf ("Para agregar las primary keys, escriba el nùmero 4\n")
+fmt.Printf ("Para agregar las foreign keys, escriba el nùmero 5\n")
+fmt.Printf ("Para borrar las Primary Keys y las Foreign Keys 6\n")
+fmt.Printf ("Para realizar la inscripciòn a una materia, escriba el nùmero 7\n")
+fmt.Printf ("Para salir, escriba el nùmero 8\n")
+
+var opcion int
+fmt.Scanf("%d",&opcion)
+return opcion
 }
 
 func ejecutarPrograma() {
-	fmt.Printf ("¡Bienvenido!\n")
-	
-	for {	
-		clear()
-		
-		opcion := mostrarOpciones()
-		
-		clear()
-		
-		switch opcion {
-		case 1:
-			createDatabase()
-			
-		case 2:
-			createDbTables()
-			
-		case 3:
-			levantarJSons()
-			
-		case 4:
-			createDbTables()
-			
-		case 5:
-			createDbTables()
-			
-		case 6:
-			inscripcionMateria()
-			
-		case 7:
-			fmt.Printf("¡Hasta la proxima!\n")
-			os.Exit(0)
-		default:
-			fmt.Printf("Opcion no valida\n")
-		}
-		
-		if !preguntarContinuar() {
-			break
-		}
-	}
+fmt.Printf ("¡Bienvenido!\n")
+
+for {
+clear()
+
+opcion := mostrarOpciones()
+
+clear()
+
+switch opcion {
+case 1:
+createDatabase()
+
+case 2:
+createDbTables()
+
+case 3:
+levantarJSons()
+
+case 4:
+agregarPrimaryKey ()
+
+case 5:
+agregarForeignKey ()
+
+case 6:
+borrarKeys ()
+
+case 7:
+inscripcionMateria()
+
+case 8:
+fmt.Printf("¡Hasta la proxima!\n")
+os.Exit(0)
+default:
+fmt.Printf("Opcion no valida\n")
+}
+
+if !preguntarContinuar() {
+break
+}
+}
 }
 
 func preguntarContinuar() bool {
-	var continuar string
-	fmt.Printf("¿Desea realizar otra accion? (s/n):\n")
-	fmt.Scanf("%s",&continuar)
-	return continuar == "s"
+var continuar string
+fmt.Printf("¿Desea realizar otra accion? (s/n):\n")
+fmt.Scanf("%s",&continuar)
+return continuar == "s"
 }
 
 func clear() {
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-	}
+cmd := exec.Command("clear")
+cmd.Stdout = os.Stdout
+cmd.Run()
+}
 
 func createDatabase() {
-	db,err := sql.Open("postgres", "user=postgres host=localhost dbname=postgres sslmode=disable")
-	if err!= nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	
-	_, err = db.Exec(`drop database if exists garcia_montoro_moralez_rodriguez_db1;`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create database garcia_montoro_moralez_rodriguez_db1;`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	fmt.Printf("Base de datos creada.\n")
+db,err := sql.Open("postgres", "user=postgres host=localhost dbname=postgres sslmode=disable")
+if err!= nil {
+log.Fatal(err)
+}
+defer db.Close()
+
+_, err = db.Exec(`drop database if exists garcia_montoro_moralez_rodriguez_db1;`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`create database garcia_montoro_moralez_rodriguez_db1;`)
+if err != nil {
+log.Fatal(err)
+}
+
+fmt.Printf("Base de datos creada.\n")
 }
 
 func createDbTables() {
-	db,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
-	if err!= nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	
-	_, err = db.Exec(`create table alumne(id_alumne int, nombre char(64), apellido char(64), dni int, fecha_nacimiento date, telefono char(64), email char(64))`)
-	if err != nil {
-		log.Fatal(err)
-	}
-		
-	_, err = db.Exec(`create table materia(id_materia int, nombre char(64))`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create table correlatividad(id_materia char(12), nombre char(64))`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create table comision(id_materia int, id_comision int, cupo int)`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create table cursada(id_materia int, id_alumne int, id_comision int, f_inscripcion timestamp, nota int, estado char(12))`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create table periodo(semestre char(12), estado char(12))`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create table historia_academica(ad_alumne int, semestre text, id_materia int, id_comision int, estado char(15), nota_regular int, nota_final int)`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create table error(id_error int, operacion char(15), semestre text, id_alumne int, id_materia int, id_comision int, f_error timestamp, motivo char(64))`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create table envio_mail(id_email int, f_generacion timestamp, email_alumne text, asunto text, cuerpo text, f_envio timestamp, estado char(10))`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = db.Exec(`create table entrada_trx(id_orden int, operacion char(15), año int, nro_semestre int, id_alumne int, id_comision int)`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	fmt.Printf("Tablas cargadas.\n")
-}	
-
-/*func tablaExiste(tableName string) bool {
 db,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
 if err!= nil {
 log.Fatal(err)
 }
-    var exists bool
-    query := `SELECT EXISTS (
-        SELECT FROM information_schema.tables
-        WHERE table_schema = 'public' AND table_name = $1
-    )`
-    err := db.QueryRow(query, tableName).Scan(&exists)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return exists
-}
+defer db.Close()
 
-func agregarPrimaryKey (){
-dbPrueba,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
-if err!= nil {
-log.Fatal(err)
-}
-defer dbPrueba.Close()
-
-fmt.Printf("Ingrese el nombre de la tabla: ")
-    var tableName string
-    _, err = fmt.Scanln(&tableName)
-    if err != nil {
-        log.Fatal(err)
-    }
-    if !tablaExiste(tableName) {
-        fmt.Printf("La tabla '%s' no existe en la base de datos.\n", tableName)
-        return
-    }
- 
-   
-fmt.Printf("ingrese las primary keys:")
-var primaryKeys string
-_, err := fmt.Scanf(&primaryKeys)
+_, err = db.Exec(`create table alumne(id_alumne int, nombre char(64), apellido char(64), dni int, fecha_nacimiento date, telefono char(64), email char(64))`)
 if err != nil {
 log.Fatal(err)
 }
-keys := strings.Split(primaryKeys, ",")
 
-for _, key := range keys {
-        key = strings.TrimSpace(key)
-        var exists bool
-        query := fmt.Sprintf("SELECT EXISTS(SELECT 1 FROM %s WHERE id = $1)", tableName)
-        if err != nil {
-            log.Fatal(err)
-        }
-        if exists {
-            fmt.Printf("La clave primaria '%s' existe en la tabla.\n", key)
-        } else {
-            fmt.Printf("La clave primaria '%s' NO existe en la tabla.\n", key)
-        }
-    }
+_, err = db.Exec(`create table materia(id_materia int, nombre char(64))`)
+if err != nil {
+log.Fatal(err)
 }
 
-func agregarForeignKey() {
-	dbPrueba,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
-	if err!= nil {
-	log.Fatal(err)
-	}
-	defer dbPrueba.Close()
-		
-	var tableName string
-	fmt.Printf("Ingrese el nombre de la tabla: ")
-	_, err = fmt.Scanln(tableName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	if !tablaExiste(tableName) {
-		fmt.Printf("La tabla '%s' no existe en la base de datos.\n", tableName)
-		return
-	}
-	
-	var foreignKey string
-	fmt.Printf("Ingrese el nombre de la foreign key: ")
-	_, err = fmt.Scanln(foreignKey)
-	if err != nil {
-		fmt.Printf("Error al leer la foreign key a ingresar. ", err)
-		return
-	}
-	
-	var externalTable string
-	fmt.Printf("Ingrese el nombre de la tabla a la que hace referencia: ")
-	_, err = fmt.Scanln(externalTable)
-	if err != nil {
-		fmt.Printf("Error al leer la tabla a la que hace referenca. ", err)
-		return
-	}
-	if !tablaExiste(externalTable) {
-		fmt.Printf("La tabla externa ingresada '%s' no existe.\n", externalTable)
-		return
-	}
-	
-	var externalColumn string
-	fmt.Printf("Ingrese el nombre de la columna externa a la que hace referencia: ")
-	_, err = fmt.Scanln(externalColumn)
-	if err != nil {
-		fmt.Printf("Error al leer la columna externa. ", err)
-		return
-	}
-	
-	sqlStatement := fmt.Sprintf(`
-		ALTER TABLE %s
-		ADD CONSTRAINT %s_%s_fk
-		FOREIGN KEY (%s)
-		REFERENCES %s(%s);
-	`,
-	tableName, tableName, foreignKey, foreignKey, externalTable, externalColumn)
-	
-	_, err = db.Exec(sqlStatement)
-	if err != nil {
-		fmt.Printf("Error al agregar la FK a la tabla. ", err)
-		return
-	}
-	
-	fmt.Printf("Foreign key '%s' agregada exitosamente a la tabla '%s'.\n", foreignKey,tableName)
-}*/
+_, err = db.Exec(`create table correlatividad(id_materia int, id_materia_correlativa int)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`create table comision(id_materia int, id_comision int, cupo int)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`create table cursada(id_materia int, id_alumne int, id_comision int, f_inscripcion timestamp, nota int, estado char(12))`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`create table periodo(semestre char(12), estado char(12))`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`create table historia_academica(id_alumne int, semestre text, id_materia int, id_comision int, estado char(15), nota_regular int, nota_final int)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`create table error(id_error int, operacion char(15), semestre text, id_alumne int, id_materia int, id_comision int, f_error timestamp, motivo char(64))`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`create table envio_mail(id_email int, f_generacion timestamp, email_alumne text, asunto text, cuerpo text, f_envio timestamp, estado char(10))`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`create table entrada_trx(id_orden int, operacion char(15), año int, nro_semestre int, id_alumne int, id_comision int)`)
+if err != nil {
+log.Fatal(err)
+}
+
+fmt.Printf("Tablas cargadas.\n")
+}
+
+func agregarPrimaryKey (){
+db,err := sql.Open("postgres", "user=postgres host=localhost dbname=prueba sslmode=disable")
+if err!= nil {
+log.Fatal(err)
+}
+defer db.Close()
+
+_, err = db.Exec(`alter table alumne add constraint pk_alumne primary key (id_alumne)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table materia add constraint pk_materia primary key (id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table correlatividad add constraint pk_correlatividad primary key (id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table comision add constraint pk_comision primary key (id_materia, id_comision)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table cursada add constraint pk_cursada primary key (id_materia, id_alumne)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table periodo add constraint pk_periodo primary key (semestre)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table historia_academica add constraint pk_academica primary key (id_alumne, semestre, id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table error add constraint pk_error primary key (id_error)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table envio_mail add constraint pk_envio_mail primary key (id_email)`)
+if err != nil {
+log.Fatal(err)
+}
+
+fmt.Printf("Primary Keys cargadas.\n")
+
+}
+
+func agregarForeignKey (){
+db,err := sql.Open("postgres", "user=postgres host=localhost dbname=prueba sslmode=disable")
+if err!= nil {
+log.Fatal(err)
+}
+defer db.Close()
+
+_, err = db.Exec(`alter table correlatividad add constraint fk_materia foreign key (id_materia) references materia (id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table correlatividad add constraint fk_materia foreign key (id_mat_correlativa) references materia (id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table comision add constraint fk_materia foreign key (id_materia) references materia (id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table cursada add constraint fk_materia foreign key (id_materia) references materia (id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table cursada add constraint fk_alumne foreign key (id_alumne) references alumne (id_alumne)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table cursada add constraint fk_comision foreign key (id_comision) references materia (id_comision)`)
+if err != nil {
+log.Fatal(err)
+}
+
+
+
+_, err = db.Exec(`alter table historia_academica add constraint fk_alumne foreign key (id_alumne) references alumne (id_alumne)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table historia_academica add constraint fk_periodo foreign key (semestre) references periodo (semestre)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table historia_academica add constraint fk_materia foreign key (id_materia) references materia (id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table historia_academica add constraint fk_comision foreign key (id_comision) references materia (id_comision)`)
+if err != nil {
+log.Fatal(err)
+}
+
+
+
+_, err = db.Exec(`alter table error add constraint fk_alumne foreign key (id_alumne) references alumne (id_alumne)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table error add constraint fk_periodo foreign key (semestre) references periodo (semestre)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table error add constraint fk_materia foreign key (id_materia) references materia (id_materia)`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table error add constraint fk_comision foreign key (id_comision) references materia (id_comision)`)
+if err != nil {
+log.Fatal(err)
+}
+
+fmt.Printf("Foreign Keys cargadas.\n")
+
+}
+
+func borrarKeys (){
+db,err := sql.Open("postgres", "user=postgres host=localhost dbname=prueba sslmode=disable")
+if err!= nil {
+log.Fatal(err)
+}
+defer db.Close()
+
+_, err = db.Exec(`alter table alumne drop constraint pk_alumne`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table materia drop constraint pk_materia`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table correlatividad drop constraint pk_correlatividad`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table correlatividad drop constraint fk_materia`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table comision drop constraint pk_comision`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table comision drop constraint fk_materia`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table cursada drop constraint pk_cursada`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table cursada drop constraint fk_materia`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table cursada drop constraint fk_alumne`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table cursada drop constraint fk_comision`)
+if err != nil {
+log.Fatal(err)
+}
+
+
+_, err = db.Exec(`alter table periodo drop constraint pk_periodo`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table historia_academica drop constraint pk_academica`)
+if err != nil {
+log.Fatal(err)
+}
+_, err = db.Exec(`alter table historia_academica drop constraint fk_materia`)
+if err != nil {
+log.Fatal(err)
+}
+_, err = db.Exec(`alter table historia_academica drop constraint fk_alumne`)
+if err != nil {
+log.Fatal(err)
+}
+_, err = db.Exec(`alter table historia_academica drop constraint fk_comision`)
+if err != nil {
+log.Fatal(err)
+}
+_, err = db.Exec(`alter table historia_academica drop constraint fk_periodo`)
+if err != nil {
+log.Fatal(err)
+}
+
+
+
+_, err = db.Exec(`alter table error drop constraint pk_error`)
+if err != nil {
+log.Fatal(err)
+}
+_, err = db.Exec(`alter table error drop constraint fk_materia`)
+if err != nil {
+log.Fatal(err)
+}
+_, err = db.Exec(`alter table error drop constraint fk_alumne`)
+if err != nil {
+log.Fatal(err)
+}
+_, err = db.Exec(`alter table error drop constraint fk_comision`)
+if err != nil {
+log.Fatal(err)
+}
+_, err = db.Exec(`alter table error drop constraint fk_periodo`)
+if err != nil {
+log.Fatal(err)
+}
+
+_, err = db.Exec(`alter table envio_mail drop constraint pk_envio_mail`)
+if err != nil {
+log.Fatal(err)
+}
+
+fmt.Printf("Primary Keys y Foreign Keys borradas.\n")
+}
+
+
 
 func levantarJSons() {
 	db,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
