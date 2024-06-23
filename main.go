d8c2c2634f3ec029707248160cc0653790907b45
MatiasMoralezC <matiasmoralezc@gmail.com>
2024-06-22 21:16:02 -0300

    Triggers para envio de mails

--- main.go
+++ main.go
@@ -451,6 +451,12 @@ func loadAllStoredProcedures() {
 	loadAperturaInscripcion()
 	loadAplicacionDeCupos()
 	loadIngresoNota()
+	
+	loadEmailAltaInscripcion()
+	loadEmailBajaInscripcion()
+	loadEmailAplicacionCupos()
+	loadEmailInscripcionEnEspera()
+	loadEmailCierreCursada()
 }
 
 func loadInscripcionMateria() {
@@ -838,6 +844,248 @@ func loadIngresoNota() {
 	if err!= nil{
 		log.Fatal(err)
 	}
+}
+
+//Se dispara cuando una inscripcion es registrada
+func loadEmailAltaInscripcion() {
+	db,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
+	if err!= nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	query := `
+	create or replace function email_alta_inscipcion() returns trigger as $$
+	declare
+		v_materia_nombre text;
+		v_comision_numero text;
+		v_alumne_nombre text;
+		v_alumne_apellido text;
+		v_email_alumne text;
+	begin
+		select nombre into v_materia_nombre from materia where id_materia = NEW.id_materia;
+		select id_comision into v_comision_numero from comision where id_materia = NEW.id_materia and id_comision = NEW.id_comision;
+		select nombre, apellido, email into v_alumne_nombre, v_alumne_apellido, v_email_alumne from alumne where id_alumne = NEW.id_alumne;
+		
+		insert into envio_email (f_generacion, email_alumne, asunto, cuerpo, f_envio, estado)
+		values (current_timestamp, v_email_alumne, 'Inscripcion registrada', 
+		'Hola ' || alumne_nombre || ' ' || alumne_apellido || ', tu inscripcion a la materia ' || materia_nombre || ', comision ' || comision_numero ||' ha sido registrada.',
+		null, 'pendiente' 
+		);
+				
+		return old;
+	end;
+	$$ language plpgsql;
+	
+	create trigger email_alta_inscripcion_trg
+	after insert on cursada
+	for each row
+	when (NEW.estado = 'aceptade')
+	execute function email_alta_inscipcion();
+	`
+	
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Trigger 'email alta inscipcion' creado con exito")
+}
+
+//Se dispara cuando la inscripcion es dada de baja
+func loadEmailBajaInscripcion() {
+	db,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
+	if err!= nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	query := `
+	create or replace function email_baja_inscripcion() returns trigger as $$
+	declare
+		v_materia_nombre text;
+		v_comision_numero text;
+		v_alumne_nombre text;
+		v_alumne_apellido text;
+		v_email_alumne text;
+	begin
+		select nombre into v_materia_nombre from materia where id_materia = OLD.id_materia;
+		select id_comision into v_comision_numero from comision where id_materia = OLD.id_materia and id_comision = OLD.id_comision;
+		select nombre, apellido, email into v_alumne_nombre, v_alumne_apellido, v_email_alumne from alumne where id_alumne = OLD.id_alumne;
+		
+		insert into envio_email (f_generacion, email_alumne, asunto, cuerpo, f_envio, estado)
+		values (current_timestamp, v_email_alumne, 'Inscripcion dada de baja',
+		'Hola ' || alumne_nombre || ' ' || alumne_apellido || ', tu inscripcion a la materia ' || materia_nombre || ', comision ' || comision_numero ||' ha sido dada de baja.',
+		null, 'pendiente'
+		);
+		
+		return old;
+	end;
+	$$ lenguage plpgsql;
+		
+	create trigger email_baja_inscripcion_trg
+	after update on cursada
+	for each row
+	when (NEW.estado = 'dade de baja')
+	execute function email_baja_inscripcion();
+	`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Trigger 'email baja inscipcion' creado con exito")
+	
+}
+
+//Se dispara cuando la inscripcion es aceptada
+func loadEmailAplicacionCupos(db *sql.DB) {
+	db,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
+	if err!= nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	query := `
+	create or replace function email_aplicacion_cupos() returns trigger as $$
+	declare
+		v_materia_nombre text;
+		v_comision_numero text;
+		v_alumne_nombre text;
+		v_alumne_apellido text;
+		v_email_alumne text;
+		v_estado_inscripcion char(12);
+	begin
+		select nombre into v_materia_nombre from materia where id_materia = NEW.id_materia;
+		select id_comision into v_comision_numero from comision where id_materia = NEW.id_materia and id_comision = NEW.id_comision;
+		select nombre, apellido, email into v_alumne_nombre, v_alumne_apellido, v_email_alumne from alumne where id_alumne = NEW.id_alumne;
+		
+		if new.estado = 'aceptade' then
+			v_estado_inscripcion := 'Inscripcion aceptada'
+		else
+			v_estado_inscripcion := 'Inscripcion en espera'
+		end if;
+		
+		insert into envio_email (f_generacion, email_alumne, asunto, cuerpo, f_envio, estado)
+		values (current_timestamp, v_email_alumne, v_estado_inscripcion,
+		'Hola ' || alumne_nombre || ' ' || alumne_apellido || ', tu inscripcion a la materia ' || materia_nombre || ', comision ' || comision_numero ||' ha sido: ' ||  || 'v_estado_inscripcion.',
+		null, 'pendiente'
+		);
+		
+		return new;
+	end;
+	$$ language plpgsql;
+
+	create trigger email_aplicacion_cupos_trg
+	after update on cursada
+	for each row
+	when (OLD.estado = 'en espera' and NEW.estado = 'aceptade')
+	execute	function email_aplicacion_cupos();
+	`
+	
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Trigger 'email aplicacion de cupo' creado con exito")
+}
+
+
+// Se dispara cuando la inscripcion en espera pasa a aceptada
+func loadEmailInscripcionEnEspera(db *sql.DB) {
+	db,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
+	if err!= nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	query := `
+	create or replace function email_inscripcion_lista_espera() return trigger as $$
+	declare
+		v_materia_nombre text;
+		v_comision_numero text;
+		v_alumne_nombre text;
+		v_alumne_apellido text;
+		v_email_alumne text;
+	begin
+		select nombre into v_materia_nombre from materia where id_materia = NEW.id_materia;
+		select id_comision into v_comision_numero from comision where id_materia = NEW.id_materia and id_comision = NEW.id_comision;
+		select nombre, apellido, email into v_alumne_nombre, v_alumne_apellido, v_email_alumne from alumne where id_alumne = NEW.id_alumne;
+		
+		insert into envio_email (f_generacion, email_alumne, asunto, cuerpo, f_envio, estado)
+		values (current_timestamp, v_email_alumne, 'Inscripcion aceptada',
+		'Hola ' || alumne_nombre || ' ' || alumne_apellido || ', tu inscripcion a la materia ' || materia_nombre || ', comision ' || comision_numero ||' ha sido finalmente aceptada',
+		null, 'pendiente'
+		);
+	
+		return new;
+	end;
+	$$ language plpgsql;
+	
+	create trigger email_inscripcion_lista_espera_trg
+	after update on cursada
+	for each row
+	when (OLD.estado = 'en espera' and NEW.estado = 'aceptade')
+	execute function email_inscripcion_lista_espera();
+	`
+	
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Trigger 'email inscripcion en espera' creado con exito")
+}
+
+//Se ejecuta cuando se cierra la cursada de una comision_numero
+func loadEmailCierreCursada(db *sql.DB) {
+	db,err := sql.Open("postgres", "user=postgres host=localhost dbname=garcia_montoro_moralez_rodriguez_db1 sslmode=disable")
+	if err!= nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	query := 	`
+	create or replace function email_cierre_cursada() returns trigger as $$
+	declare
+		v_materia_nombre text;
+		v_comision_numero text;
+		v_alumne_nombre text;
+		v_alumne_apellido text;
+		v_email_alumne text;
+		v_estado_academico char(15);
+		v_nota_regular int;
+		v_nota_final int;
+		v_semestre_actual text;
+	begin
+		select nombre into v_materia_nombre from materia where id_materia = NEW.id_materia;
+		select id_comision into v_comision_numero from comision where id_materia = NEW.id_materia and id_comision = NEW.id_comision;
+		select nombre, apellido, email into v_alumne_nombre, v_alumne_apellido, v_email_alumne from alumne where id_alumne = NEW.id_alumne;
+		select semestre into v_semestre_actual from periodo where estado = 'cursada';
+		select estado, nota_regular, nota_final into v_estado_academico, v_nota_regular, v_nota_final from historia_academica
+		where id_alumne = NEW.id_alumne and id_materia = NEW.id_materia and semestre = v_semestre_actual;
+		
+		insert into envio_email (f_generacion, email_alumne, asunto, cuerpo, f_envio, estado)
+		values (current_timestamp, 'Cierre de cursada',
+		'Hola ' || alumne_nombre || ' ' || alumne_apellido || ', tu inscripcion a la materia ' || materia_nombre || ', comision ' || comision_numero ||
+		' ha sido cerrada. Estado: ' || v_estado_academico || ', Nota regular: ' || COALESCE(v_nota_regular::text, 'N/A') || ', Nota final: ' || COALESCE(v_nota_final::text, 'N/A') || '.',
+		null, 'pendiente'
+		);
+		
+		return new;
+	end;
+	
+	$$ language plpgsql;
+	
+	create trigger email_cierre_cursada_trg
+	after update on cursada
+	for each row
+	when (NEW.estado = 'cerrado')
+	execute function email_cierre_cursada();
+	`
+	
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Trigger 'email cierre cursada' creado con exito")
 }
 
 
